75538a7baa0a618ef832936181da1651745ecab1
Dorsa R <dorsa.rohani@gmail.com>
2023-07-05 13:20:46 -0400

    Update superposition.py

--- Superposition of Transverse Waves/superposition.py
+++ Superposition of Transverse Waves/superposition.py
@@ -1,5 +1,3 @@
-# Code for the app
-
 import numpy as np
 import matplotlib.pyplot as plt
 import mecsimcalc as msc
@@ -13,27 +11,43 @@ def main(inputs):
     frequency1 = inputs['frequency_1']
     amplitude2 = inputs['amplitude_2']
     frequency2 = inputs['frequency_2']
+    phase1 = inputs['phase_constant_1']
+    phase2 = inputs['phase_constant_2']
 
-    # computing each wave's y values
-    wave1 = amplitude1 * np.sin(2*np.pi*frequency1*t) # wave 1's y values
-    wave2 = amplitude2 * np.sin(2*np.pi*frequency2*t) # wave 2's y values
+    if phase1 == 0:
+        # computing each wave's y values
+        wave1 = amplitude1 * np.sin(2*np.pi*frequency1*t) # wave 1's y values
+        equation = f"y = {amplitude1}sin({frequency1}x) + y = {amplitude2}sin({frequency2}x)"
+    else:
+        wave1 = amplitude1 * np.sin(2*np.pi*frequency1*t + phase1) # wave 1's y values
+        equation = f"y = {amplitude1}sin({frequency1}x + phase1) + y = {amplitude2}sin({frequency2}x + phase1)"
+
+    if phase2 == 0:
+        # computing each wave's y values
+        wave2 = amplitude2 * np.sin(2*np.pi*frequency2*t) # wave 2's y values
+        equation = f"y = {amplitude1}sin({frequency1}x) + y = {amplitude2}sin({frequency2}x)"
+    else:
+        wave2 = amplitude2 * np.sin(2*np.pi*frequency2*t + phase2) # wave 1's y values
+        equation = f"y = {amplitude1}sin({frequency1}x + phase1) + y = {amplitude2}sin({frequency2}x + phase2)"
+
     superposition = wave1 + wave2 # y values of resulting function (superposition)
 
     # Plot the x (t) and y (superposition) values with a label "superposition"
     plt.plot(t, superposition, label="Superposition")
+    plt.plot(t, wave1)
+    plt.plot(t, wave2)
 
     # Construct the title, labels and legend
-    equation = f"y = {amplitude1} * sin({frequency1} * x) + y = {amplitude2} * sin({frequency2} * x)"
-    plt.title(equation)
+    plt.title("Superposition")
     plt.xlabel("Time")
-    plt.ylabel("Superposition of Waves")
+    plt.ylabel("Superposition of the waves")
     plt.legend()
 
     # Get plot image and download link using mecsimcalc's print_plot function
     img, download = msc.print_plot(plt, download=True)
 
     return {
-        "plot": img,
         "equation": equation,
+        "plot": img,
         "download": download,
     }
